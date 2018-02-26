4eff3857c0560e07e22f450bc36a774f1993d2b9
Elizabeth Wong <lizzwong@gmail.com>
2018-02-25 21:51:27 -0600

    heroku stuff

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,34 @@
 const pg = require('pg');
-const Pool = pg.Pool;
+const url = require('url');
+let config = {};
 
-const config = {
-    database: 'weekend4',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 5000
-};
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
 
-const pool = new Pool(config);
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true, // heroku requires ssl to be true
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
 
+} else {
+    config = {
+        user: process.env.PG_USER || null, //env var: PGUSER
+        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+        port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+        database: process.env.DATABASE_NAME || 'weekend4', //env var: PGDATABASE
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+}
 
-
-module.exports = pool;
+module.exports = new pg.Pool(config);
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 8888;
+const PORT = process.env.PORT ||  8888;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
